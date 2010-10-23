a67eed62825c581bb68ee9515af70cd841c23138
Flávio Ribeiro <email@flavioribeiro.com>
2010-10-23 13:40:54 -0300

    Add a simple config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# cocoslides - a slides framework built in Cocos2d
+# by Flávio Ribeiro (email@flavioribeiro.com)
+
+#TODO Need comments, explanations on how to create and build the slides
+# etc.
+
+'''
+MEDIA_PATH: Here you can set the media path. Media path contents are images, sounds, videos, etc.
+'''
+MEDIA_PATH = ''
+
+
